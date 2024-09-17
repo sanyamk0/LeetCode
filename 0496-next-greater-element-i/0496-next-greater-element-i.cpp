b374102ac69c975b540aa3ce61b5902a20ab138b
Sanyam Kumar <21U03010@iiitbhopal.ac.in>
2024-09-17 22:43:45 +0530

    Time: 18 ms (5.37%), Space: 11 MB (95.39%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,24 +1,32 @@
 class Solution {
-public:
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size(), n2 = nums2.size();
-        vector<int> ans;
-        for (int i = 0; i < n1; i++) {
-            for (int j = 0; j < n2; j++) {
-                if (nums1[i] == nums2[j]) {
-                    int flag = 0;
-                    for (int k = j; k < n2; k++) {
-                        if (nums2[k] > nums2[j]) {
-                            ans.push_back(nums2[k]);
-                            flag = 1;
-                            break;
-                        }
-                    }
-                    if (!flag)
-                        ans.push_back(-1);
-                }
+private:
+    vector<int> nextEl(vector<int> v) {
+        int n = v.size();
+        vector<int> ans(n);
+        stack<int> st;
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && st.top() < v[i])
+                st.pop();
+            if (st.empty()) {
+                ans[i] = -1;
+                st.push(v[i]);
+            } else {
+                ans[i] = st.top();
+                st.push(v[i]);
             }
         }
         return ans;
+    }
+
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> v;
+        int n = nums1.size(), m = nums2.size();
+        vector<int> ans = nextEl(nums2);
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                if (nums1[i] == nums2[j])
+                    v.push_back(ans[j]);
+        return v;
     }
 };
