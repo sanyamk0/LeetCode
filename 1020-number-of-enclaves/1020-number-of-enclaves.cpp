67f6bbf529d4460b5bf9d407f882bb461639d3d2
Sanyam Kumar <21U03010@iiitbhopal.ac.in>
2024-10-13 09:36:56 +0530

    Time: 27 ms (99.97%), Space: 32.7 MB (61.5%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
-    void dfs(int r, int c, vector<vector<int>>& vis, vector<vector<int>>& grid,
+    void bfs(int r, int c, vector<vector<int>>& vis, vector<vector<int>>& grid,
              int m, int n) {
+        queue<pair<int, int>> q;
+        q.push({r, c});
         vis[r][c] = 1;
         int delRow[] = {-1, 0, 1, 0};
         int delCol[] = {0, 1, 0, -1};
-        for (int i = 0; i < 4; i++) {
-            int nRow = r + delRow[i];
-            int nCol = c + delCol[i];
-            if (nRow >= 0 && nRow < m && nCol >= 0 && nCol < n &&
-                !vis[nRow][nCol] && grid[nRow][nCol] == 1) {
-                dfs(nRow, nCol, vis, grid, m, n);
+        while (!q.empty()) {
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            for (int i = 0; i < 4; i++) {
+                int nRow = row + delRow[i];
+                int nCol = col + delCol[i];
+                if (nRow >= 0 && nRow < m && nCol >= 0 && nCol < n &&
+                    !vis[nRow][nCol] && grid[nRow][nCol] == 1) {
+                    vis[nRow][nCol] = 1;
+                    q.push({nRow, nCol});
+                }
             }
         }
     }
@@ -24,15 +32,15 @@ public:
                     totalOnes++;
         for (int r = 0; r < m; r++) {
             if (grid[r][0] == 1 && !vis[r][0])
-                dfs(r, 0, vis, grid, m, n);
+                bfs(r, 0, vis, grid, m, n);
             if (grid[r][n - 1] == 1 && !vis[r][n - 1])
-                dfs(r, n - 1, vis, grid, m, n);
+                bfs(r, n - 1, vis, grid, m, n);
         }
         for (int c = 0; c < n; c++) {
             if (grid[0][c] == 1 && !vis[0][c])
-                dfs(0, c, vis, grid, m, n);
+                bfs(0, c, vis, grid, m, n);
             if (grid[m - 1][c] == 1 && !vis[m - 1][c])
-                dfs(m - 1, c, vis, grid, m, n);
+                bfs(m - 1, c, vis, grid, m, n);
         }
         for (int r = 0; r < m; r++)
             for (int c = 0; c < n; c++)
