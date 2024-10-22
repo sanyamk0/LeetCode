4d3bedb0690e6b1df572969ce6427c37d99dafd9
Sanyam Kumar <21U03010@iiitbhopal.ac.in>
2024-10-22 15:30:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Sub-arrays with equal number of occurences/subarrays-with-equal-number-of-occurences.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    int sameOccurrence(vector<int>& arr, int x, int y) {
+       unordered_map<int, int> countMap;
+        int countX = 0, countY = 0, result = 0;
+        countMap[0] = 1;
+        for (int num : arr) {
+            if (num == x)
+                countX++;
+            else if (num == y)
+                countY++;
+            int diff = countX - countY;
+            result += countMap[diff];
+            countMap[diff]++;
+        }
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int x = stoi(ks);
+        string ks1;
+        getline(cin, ks1);
+        int y = stoi(ks1);
+        Solution ob;
+        int ans = ob.sameOccurrence(arr, x, y);
+        cout << ans << "\n";
+        // cout<<"~"<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
