d471b7689ef0c7aef0a473b3b38df96ca4a687e4
Sanyam Kumar <21U03010@iiitbhopal.ac.in>
2024-11-03 20:43:26 +0530

    Time: 0 ms (100%), Space: 7.6 MB (55.81%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.size() != goal.size())
+            return false;
+        string doubled = s + s;
+        return doubled.find(goal) != string::npos;
+    }
+};
