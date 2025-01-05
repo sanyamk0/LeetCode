427a6ecc5e335c949d8f94bff070aa18575f468e
Sanyam Kumar <21U03010@iiitbhopal.ac.in>
2025-01-05 15:38:48 +0530

    Time: 0 ms (100%), Space: 23.9 MB (12.16%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0, j = 0;
+        while (j < n) {
+            if (nums[j] != 0)
+                swap(nums[i++], nums[j++]);
+            else
+                j++;
+        }
+    }
+};
