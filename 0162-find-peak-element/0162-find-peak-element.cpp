5d3356a74c85c5a797a813fcabe3986d9ec56aaa
Sanyam Kumar <21U03010@iiitbhopal.ac.in>
2025-01-09 20:34:25 +0530

    Time: 4 ms (0.77%), Space: 13.6 MB (20.27%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -2,14 +2,10 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         int n = nums.size();
-        int maxi = INT_MIN;
-        int index = -1;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] >= maxi) {
-                index = i;
-                maxi = nums[i];
-            }
-        }
-        return index;
+        unordered_map<int, int> mp;
+        for (int i = 0; i < n; i++)
+            mp[nums[i]] = i;
+        sort(nums.begin(), nums.end());
+        return mp[nums[n - 1]];
     }
 };
