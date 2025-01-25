89a4bb8d654e0879ef1049970119014f2b3f9912
Sanyam Kumar <21U03010@iiitbhopal.ac.in>
2025-01-25 22:31:54 +0530

    Time: 6 ms (73.72%), Space: 10.2 MB (99.66%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,15 +9,19 @@
 class Solution {
 public:
     ListNode* detectCycle(ListNode* head) {
-        unordered_set<ListNode*> st;
-        ListNode* curr = head;
-        while (curr) {
-            if (st.find(curr) != st.end()) {
-                return curr;
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
             }
-            st.insert(curr);
-            curr = curr->next;
         }
-        return NULL;
+        return nullptr;
     }
 };
