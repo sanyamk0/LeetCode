9edca86ed419376b36c229cf495104a442251393
Sanyam Kumar <21U03010@iiitbhopal.ac.in>
2025-01-28 15:10:41 +0530

    Time: 1790 ms (7.74%), Space: 14.6 MB (87.5%) - LeetHub

--- 1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.cpp
+++ 1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.cpp
@@ -3,16 +3,15 @@ public:
     int numTriplets(vector<int>& nums1, vector<int>& nums2) {
         int n1 = nums1.size(), n2 = nums2.size();
         int cnt = 0;
-        long long sq=1;
-        long long pr=1;
+        long long sq = 1;
+        long long pr = 1;
         for (int i = 0; i < n1; i++) {
             for (int j = 0; j < n2; j++) {
                 for (int k = j + 1; k < n2; k++) {
-                     sq = static_cast<long long>(nums1[i]) * nums1[i];
-                     pr = static_cast<long long>(nums2[j]) * nums2[k];
+                    sq = static_cast<long long>(nums1[i]) * nums1[i];
+                    pr = static_cast<long long>(nums2[j]) * nums2[k];
                     if (sq == pr) {
                         cnt++;
-                        // sq = 1, pr = 1;
                     }
                 }
             }
@@ -20,11 +19,10 @@ public:
         for (int i = 0; i < n2; i++) {
             for (int j = 0; j < n1; j++) {
                 for (int k = j + 1; k < n1; k++) {
-                     sq = static_cast<long long>(nums2[i]) * nums2[i];
-                     pr = static_cast<long long>(nums1[j]) * nums1[k];
+                    sq = static_cast<long long>(nums2[i]) * nums2[i];
+                    pr = static_cast<long long>(nums1[j]) * nums1[k];
                     if (sq == pr) {
                         cnt++;
-                        // sq = 1, pr = 1;
                     }
                 }
             }
