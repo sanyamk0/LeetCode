3fd1567cccf348885a6f7ba54780911a58f8fbbf
Sanyam Kumar <21U03010@iiitbhopal.ac.in>
2025-02-14 02:06:37 +0530

    Time: 5 ms (9.5%), Space: 12.8 MB (8.58%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    void func(vector<vector<int>>& ans, vector<int>& ds, vector<int>& mp,
-              vector<int>& nums) {
+    void helper(vector<vector<int>>& ans, vector<int> ds, vector<int> nums,
+                vector<int>& mp) {
         if (ds.size() == nums.size()) {
             ans.push_back(ds);
             return;
         }
         for (int i = 0; i < nums.size(); i++) {
-            if (!mp[i]) {
+            if (mp[i] != 1) {
                 ds.push_back(nums[i]);
                 mp[i] = 1;
-                func(ans, ds, mp, nums);
+                helper(ans, ds, nums, mp);
                 ds.pop_back();
                 mp[i] = 0;
             }
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        int n = nums.size();
         vector<vector<int>> ans;
         vector<int> ds;
-        vector<int> mp(n, 0);
-        func(ans, ds, mp, nums);
+        vector<int> mp(nums.size(), 0);
+        helper(ans, ds, nums, mp);
         return ans;
     }
 };
