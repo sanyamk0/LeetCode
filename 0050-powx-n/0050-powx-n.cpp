e82d5edba09b944e006083f366ecb91d3557b8b8
Sanyam Kumar <21U03010@iiitbhopal.ac.in>
2025-02-13 13:25:42 +0530

    Time: 0 ms (100%), Space: 8.6 MB (71.59%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -2,10 +2,20 @@ class Solution {
 public:
     double myPow(double x, int n) {
         double ans = 1.0;
-        for (int i = 0; i < abs(n); i++)
-            ans *= x;
+        long nn = n;
+        if (nn < 0)
+            nn = -1 * nn;
+        while (nn > 0) {
+            if (nn & 1) {
+                ans *= x;
+                nn -= 1;
+            } else {
+                x *= x;
+                nn /= 2;
+            }
+        }
         if (n < 0)
-            ans = 1 / ans;
+            return 1 / ans;
         return ans;
     }
 };
