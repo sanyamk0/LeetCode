dfce428e890aa67bc716789bac32415ee07048f3
Sanyam Kumar <21U03010@iiitbhopal.ac.in>
2025-02-15 00:30:45 +0530

    Time: 48 ms (5.85%), Space: 54.4 MB (6.16%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    bool isSafe(int row, int col, vector<string> board, int n) {
+        int dupRow = row;
+        int dupCol = col;
+        // upper left direction
+        while (row >= 0 && col >= 0) {
+            if (board[row][col] == 'Q')
+                return false;
+            row--;
+            col--;
+        }
+        row = dupRow;
+        col = dupCol;
+        // left direction
+        while (col >= 0) {
+            if (board[row][col] == 'Q')
+                return false;
+            col--;
+        }
+        row = dupRow;
+        col = dupCol;
+        // lower left direction
+        while (row < n && col >= 0) {
+            if (board[row][col] == 'Q')
+                return false;
+            row++;
+            col--;
+        }
+        return true;
+    }
+    void helper(int col, vector<vector<string>>& ans, vector<string> board,
+                int n) {
+        if (col == n) {
+            ans.push_back(board);
+            return;
+        }
+        for (int row = 0; row < n; row++) {
+            if (isSafe(row, col, board, n)) {
+                board[row][col] = 'Q';
+                helper(col + 1, ans, board, n);
+                board[row][col] = '.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        string s(n, '.');
+        for (int i = 0; i < n; i++)
+            board[i] = s;
+        helper(0, ans, board, n);
+        return ans;
+    }
+};
