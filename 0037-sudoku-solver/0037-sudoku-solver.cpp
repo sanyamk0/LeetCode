abb103af7b325ce633f110650b7243b11cc5fbf2
Sanyam Kumar <21U03010@iiitbhopal.ac.in>
2025-02-15 02:33:01 +0530

    Time: 334 ms (58.48%), Space: 8.8 MB (29.22%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
-    bool isPossible(int row, int col, char c, vector<vector<char>> board) {
-        for (int i = 0; i < 9; i++) {
+    bool isPossible(int row, int col, char c, vector<vector<char>>& board) {
+        // check row
+        for (int i = 0; i < 9; i++)
             if (board[row][i] == c)
                 return false;
+        // check column
+        for (int i = 0; i < 9; i++)
             if (board[i][col] == c)
                 return false;
-            if (board[(row / 3) * 3 + i / 3][(col / 3) * 3 + i % 3] == c)
-                return false;
-        }
+        // check 3 * 3 grid
+        int startRow = row - row % 3;
+        int startCol = col - col % 3;
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                if (board[startRow + i][startCol + j] == c)
+                    return false;
         return true;
     }
     bool helper(vector<vector<char>>& board) {
