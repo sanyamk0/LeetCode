987e73b7531c46da2a5d7ad2b219a0e7798844e3
Sanyam Kumar <21U03010@iiitbhopal.ac.in>
2025-02-17 19:34:14 +0530

    Time: 3 ms (78.62%), Space: 7.9 MB (84.16%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findSequences(int charCount[26]) {
+        int totalCount = 0;
+        for (int pos = 0; pos < 26; pos++) {
+            if (charCount[pos] == 0)
+                continue;
+            totalCount++;
+            charCount[pos]--;
+            totalCount += findSequences(charCount);
+            charCount[pos]++;
+        }
+        return totalCount;
+    }
+    int numTilePossibilities(string tiles) {
+        int charCount[26] = {0};
+        for (char c : tiles)
+            charCount[c - 'A']++;
+        return findSequences(charCount);
+    }
+};
